bc786ae40e66cdd45f766feba1683e42a39cf5e1
CS-5 <me@carsonseese.com>
2020-12-23 14:21:45 -0500

    Code comments and prep for noise

--- src/commands/fun/deepfry.ts
+++ src/commands/fun/deepfry.ts
@@ -13,6 +13,7 @@ import jimpTypes from "@jimp/types";
 import jimpFisheye from "@jimp/plugin-fisheye";
 import jimp from "jimp";
 import path from "path";
+import gm from "gm";
 
 // TODO: At this point, this custom config is not required.
 // It would be nice to get the fisheye function working however, so I'm leaving it here.
@@ -78,7 +79,7 @@ export default class DeepfryCommand extends Command {
         );
 
         if (!lastMessage) {
-          return msg.say("Sorry, I can't find the last message :(");
+          return msg.say("Unable to find the last message :(");
         }
       }
 
@@ -94,7 +95,7 @@ export default class DeepfryCommand extends Command {
 
       if (!isImageUrl(url)) {
         return msg.say(
-          "Wat. I can't seem to recognize that attachment as an image D:"
+          "I can't seem to recognize that attachment as an image D:"
         );
       }
 
@@ -137,15 +138,22 @@ export default class DeepfryCommand extends Command {
     }
 
     try {
+      // This will take a while, so indicate the bot is working on it
       msg.channel.startTyping();
 
+      // Define some constants for the level of pixelation, and use of emojis
+      // All values are randomly generated
       const pixels = getRandomInt(3, 2);
       const useOkHand = getRandomBool(config.commands.fry.vars.okHandProb);
-      const numHands = getRandomInt(config.commands.fry.vars.maxHands, 1);
       const useWearyFace = getRandomBool(
         config.commands.fry.vars.wearyFaceProb
       );
+      const useHundred = getRandomBool(config.commands.fry.vars.hundredProb);
+      const useWater = getRandomBool(config.commands.fry.vars.waterProb);
 
+      // Load images based on the random generation
+      // TODO: Possible to load these a single time, rather than when the command is called, or is that a bad idea?
+      // TODO: If that is a bad idea, maybe load them during the processing on lines 188-196ish? Is that possible?
       let imgOkHand: jimp;
       if (useOkHand) {
         imgOkHand = await fryJimp.read(
@@ -160,55 +168,106 @@ export default class DeepfryCommand extends Command {
         );
       }
 
-      return msg.say(
-        "",
-        new MessageAttachment(
-          await fryJimp.read(imgUrl).then((i) => {
-            if (useOkHand) {
-              imgOkHand.scaleToFit(
-                i.getWidth() * config.commands.fry.vars.superimposeScale,
-                i.getHeight() * config.commands.fry.vars.superimposeScale
-              );
+      let imgHundred: jimp;
+      if (useHundred) {
+        imgHundred = await fryJimp.read(
+          path.join(__dirname, "../../assets/hundred.png")
+        );
+      }
 
-              for (let q = 1; q <= numHands; q++) {
-                this.superimpose(i, imgOkHand);
-              }
-            }
+      let imgWater: jimp;
+      if (useWater) {
+        imgWater = await fryJimp.read(
+          path.join(__dirname, "../../assets/sweat-droplets.png")
+        );
+      }
 
-            if (useWearyFace) {
-              imgWearyFace.scaleToFit(
-                i.getWidth() * config.commands.fry.vars.superimposeScale,
-                i.getHeight() * config.commands.fry.vars.superimposeScale
-              );
+      // Start applying image effects
+      let out = await fryJimp.read(imgUrl).then((i) => {
+        i.pixelate(pixels)
+          .posterize(config.commands.fry.vars.posterize)
+          .contrast(config.commands.fry.vars.contrast)
+          .color([
+            {
+              apply: "mix",
+              params: ["#eb4034", config.commands.fry.vars.redMixOpacity],
+            },
+          ])
+          .quality(config.commands.fry.vars.jpeg);
 
-              this.superimpose(i, imgWearyFace);
-            }
+        // Add emojis
+        if (useHundred) {
+          imgHundred.scaleToFit(
+            i.getWidth() * config.commands.fry.vars.superimposeScale,
+            i.getHeight() * config.commands.fry.vars.superimposeScale
+          );
 
-            return i
-              .pixelate(pixels)
-              .posterize(config.commands.fry.vars.posterize)
-              .contrast(config.commands.fry.vars.contrast)
-              .color([
-                {
-                  apply: "mix",
-                  params: ["#eb4034", config.commands.fry.vars.redMixOpacity],
-                },
-              ])
-              .quality(config.commands.fry.vars.jpeg)
-              .getBufferAsync(fryJimp.MIME_JPEG)
-              .then((b) => {
-                msg.channel.stopTyping();
-                return b;
-              });
-          })
-        )
-      );
+          this.superimpose(i, imgHundred);
+        }
+
+        if (useWater) {
+          imgWater.scaleToFit(
+            i.getWidth() * config.commands.fry.vars.superimposeScale,
+            i.getHeight() * config.commands.fry.vars.superimposeScale
+          );
+
+          this.superimpose(i, imgWater);
+        }
+
+        if (useOkHand) {
+          imgOkHand.scaleToFit(
+            i.getWidth() * config.commands.fry.vars.superimposeScale,
+            i.getHeight() * config.commands.fry.vars.superimposeScale
+          );
+
+          // Randomly select number of ok_hand to place
+          for (
+            let q = 1;
+            q <= getRandomInt(config.commands.fry.vars.maxHands, 1);
+            q++
+          ) {
+            this.superimpose(i, imgOkHand);
+          }
+        }
+
+        if (useWearyFace) {
+          imgWearyFace.scaleToFit(
+            i.getWidth() * config.commands.fry.vars.superimposeScale,
+            i.getHeight() * config.commands.fry.vars.superimposeScale
+          );
+
+          this.superimpose(i, imgWearyFace);
+        }
+
+        // Return buffer
+        return i.getBufferAsync(fryJimp.MIME_JPEG).then((b) => {
+          return b;
+        });
+      });
+
+      // Ideally, we will add some level of noise to the final image,
+      // but there doesn't seem to be a clear cut way to accomplish that (See below).
+      // If noise is to be added, this is where it would happen.
+
+      // gm(out)
+      //   .noise("g")
+      //   .toBuffer((e, b) => {
+      //     if (e) throw e;
+      //     out = b;
+      //     // Note: This does not work because this function is called async
+      //     // from the rest of the code (I think). If there is a way to change that,
+      //     // that would be ideal.
+      //   });
+
+      msg.channel.stopTyping();
+      return msg.say("", new MessageAttachment(out));
     } catch (e) {
       console.error(e);
       return msg.say("Unable to fry the image D:");
     }
   }
 
+  // Superimpose simply places an image at random coordinates (factoring in the size of the image being placed, I think..)
   superimpose(baseImage: jimp, srcImage: jimp) {
     baseImage.blit(
       srcImage,
