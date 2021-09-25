28a0a989250c3959f8d13110ff8f2e40ca7d47e3
JosNun <josiah.nunemaker@gmail.com>
2021-09-25 19:34:22 -0400

    Add !intent command

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -44,3 +44,6 @@ export const CommandOptionsFields = [
 export const TENOR_URL = "https://api.tenor.com/v1/random";
 
 export const INSPIRE_URL = "http://inspirobot.me/api";
+
+export const PERSPECTIVE_URL =
+  "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
