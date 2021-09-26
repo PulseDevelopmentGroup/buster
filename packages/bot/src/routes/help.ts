130d1ae92955b11ee906d6caf462e02adee7abfe
JosNun <josiah.nunemaker@gmail.com>
2021-09-26 18:51:47 -0400

    Add route to get list of commands

---
+++ packages/bot/src/routes/help.ts
@@ -0,0 +1,38 @@
+import type { PieceContext } from "@sapphire/framework";
+import {
+  ApiRequest,
+  ApiResponse,
+  methods,
+  Route,
+  RouteOptions,
+} from "@sapphire/plugin-api";
+
+export class HelpRoute extends Route {
+  public constructor(context: PieceContext, options?: RouteOptions) {
+    super(context, {
+      ...options,
+      route: "help",
+    });
+  }
+
+  public [methods.GET](_request: ApiRequest, response: ApiResponse): void {
+    const commands = this.container.stores.get("commands").reduce(
+      (acc, command, key) => {
+        command.name;
+        acc.set(key, {
+          description: command.description,
+        });
+
+        return acc;
+      },
+      new Map<
+        string,
+        {
+          description: string;
+        }
+      >(),
+    );
+
+    response.json(Object.fromEntries(commands));
+  }
+}
