5d52ce0324afac666f8fa53b689ff7d4c010f14d
JosNun <josiah.nunemaker@gmail.com>
2021-09-26 19:09:54 -0400

    Add react-query

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ReactNode } from "react";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "../lib/utils";
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 export default MyApp;
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useQuery } from "react-query";
 
 const Home: NextPage = () => {
+  const { data } = useQuery("help");
+
+  console.log(data);
+
   return (
     <div className={styles.container}>
       <Head>
