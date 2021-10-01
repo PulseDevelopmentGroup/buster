dfc76bcaf751bececd5197591ac23cb5edeb394f
CS-5 <me@carsonseese.com>
2021-10-01 18:19:57 +0000

    Polish logger

--- packages/bot/src/lib/logger.ts
+++ packages/bot/src/lib/logger.ts
@@ -1,48 +1,68 @@
 import winston from "winston";
-import { botConfig } from "./config";
+import { config } from "./config";
 
-// prettyFormat is the log formatter used when in a development scenario
-const prettyFormat = winston.format.combine(
-  winston.format.colorize(),
-  winston.format.timestamp(),
-  winston.format.printf(({ timestamp, level, service, message, ...rest }) => {
-    const extraParams = Object.entries(rest)
-      .map(([key, value]) => `${key}=${value}`)
-      .join(" ");
+export class BotLogger {
+  // prettyFormat is the log formatter used when in a development scenario
+  private prettyFormat: winston.Logform.Format = winston.format.simple();
 
-    return `${timestamp} [${level}] [${service}] ${message} ${extraParams}`;
-  }),
-);
+  // Default logging options, primarily handling logic depending on the environment
+  private defaultOptions: winston.LoggerOptions = {};
 
-// Default logging options, primarily handling logic depending on the environment
-const defaultOptions: winston.LoggerOptions = {
-  level: botConfig.env.development ? "debug" : "info",
-  format: botConfig.env.development
-    ? prettyFormat
-    : winston.format.combine(winston.format.timestamp(), winston.format.json()),
-  transports: [new winston.transports.Console()],
-};
+  // Used for general bot event logging
+  public bot = winston.createLogger();
 
-// Works much in the same way as applyConfig, just applies options for the logger
-function applyOptions(opts?: winston.LoggerOptions): winston.LoggerOptions {
-  return {
-    ...opts,
-    ...defaultOptions,
-  };
+  // Used for command logging
+  public command = winston.createLogger();
+
+  // TODO: Message logger for export to Loki/Elastic/Other
+
+  public constructor(debug: boolean) {
+    this.prettyFormat = winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.printf(
+        ({ timestamp, level, service, message, ...rest }) => {
+          const extraParams = Object.entries(rest)
+            .map(([key, value]) => `${key}=${value}`)
+            .join(" ");
+
+          return `${timestamp} [${level}] [${service}] ${message} ${extraParams}`;
+        },
+      ),
+    );
+
+    this.defaultOptions = {
+      level: debug ? "debug" : "info",
+      format: debug
+        ? this.prettyFormat
+        : winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+      transports: [new winston.transports.Console()],
+    };
+
+    this.bot.configure(
+      this.apply({
+        defaultMeta: { service: "bot" },
+      }),
+    );
+
+    this.command.configure(
+      this.apply({
+        defaultMeta: { service: "command" },
+      }),
+    );
+  }
+
+  // Works much in the same way as applyConfig, just applies options for the logger
+  private apply(opts?: winston.LoggerOptions): winston.LoggerOptions {
+    return {
+      ...opts,
+      ...this.defaultOptions,
+    };
+  }
 }
 
-// Used for general bot event logging
-export const logger = winston.createLogger(
-  applyOptions({
-    defaultMeta: { service: "bot" },
-  }),
-);
-
-// Used for command logging
-export const commandLogger = winston.createLogger(
-  applyOptions({
-    defaultMeta: { service: "command" },
-  }),
-);
-
-// TODO: Message logger for export to Loki/Elastic/Other
+// Creating an instance of logger here to be accessed globally
+export const logger = new BotLogger(config.env.development);
--- packages/bot/src/listeners/commands/commandLogger.ts
+++ packages/bot/src/listeners/commands/commandLogger.ts
@@ -1,8 +1,8 @@
 import type { ListenerOptions, PieceContext } from "@sapphire/framework";
 import { Events, Listener, Command } from "@sapphire/framework";
 import type { Message } from "discord.js";
-import { botConfig } from "../../lib/config";
-import { commandLogger } from "../../lib/logger";
+import { config } from "../../lib/config";
+import { logger } from "../../lib/logger";
 
 //TODO: Might want to consider runninng only on CommandSuccess, but for now this will fire on everything
 export class UserEvent extends Listener<typeof Events.CommandRun> {
@@ -15,7 +15,7 @@ export class UserEvent extends Listener<typeof Events.CommandRun> {
 
   // Fires on every command ran by a user
   public run(message: Message, command: Command) {
-    commandLogger.info(message.content, {
+    logger.command.info(message.content, {
       shard: (message.guild?.shardId ?? 0).toString(),
       name: command.name,
       author: `${message.author.tag}`,
@@ -25,7 +25,7 @@ export class UserEvent extends Listener<typeof Events.CommandRun> {
 
   // Only enable if logCommands is true or we are in a dev enviornment
   public onLoad() {
-    this.enabled = botConfig.env.logCommands || botConfig.env.development;
+    this.enabled = config.env.logCommands || config.env.development;
     return super.onLoad();
   }
 }
