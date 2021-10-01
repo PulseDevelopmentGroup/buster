11094e8b26e49e5cde08b6e07c64896cb6e412ec
JosNun <josiah.nunemaker@gmail.com>
2021-10-01 01:08:43 +0000

    Use embed for error reporting

--- packages/bot/src/listeners/commands/commandError.ts
+++ packages/bot/src/listeners/commands/commandError.ts
@@ -1,6 +1,7 @@
 import type { CommandErrorPayload, Events } from "@sapphire/framework";
 import { Listener } from "@sapphire/framework";
 import { send } from "@sapphire/plugin-editable-commands";
+import { MessageEmbed } from "discord.js";
 
 export class UserEvent extends Listener<typeof Events.CommandError> {
   public async run(e: Error, payload: CommandErrorPayload) {
@@ -8,7 +9,21 @@ export class UserEvent extends Listener<typeof Events.CommandError> {
       .fetch("893296394478182450")
       .then((channel) => {
         if (channel?.isText()) {
-          channel.send(e.message);
+          const embed = new MessageEmbed({
+            title: "Error Details",
+          });
+          embed.setColor("RED");
+
+          embed.addField("Message", e.message);
+          embed.addField("Error Type", e.name);
+
+          if (e.stack) {
+            embed.addField("Stack", `\`${e.stack}\``);
+          }
+
+          channel.send({
+            embeds: [embed],
+          });
         }
       });
 
