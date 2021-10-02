d4d07067d7188f9458e1bacfe5c7117c902d5297
CS-5 <me@carsonseese.com>
2021-10-02 19:34:12 -0400

    Handle multiple units

--- packages/bot/src/listeners/fun/uselessConversions.ts
+++ packages/bot/src/listeners/fun/uselessConversions.ts
@@ -4,13 +4,19 @@ import type { Message } from "discord.js";
 import { config } from "../../lib/config";
 import { NumberRegex, UnitRegex } from "../../lib/constants";
 
-enum Measurements {
-  meter = "meter",
-  liter = "liter",
-  gram = "gram",
-  second = "second",
+enum Measurement {
+  length = "length", // meter
+  volume = "volume", // liter
+  mass = "mass", // gram
+  time = "time", // second
   unknown = "unknown",
 }
+
+type Unit = {
+  input: string;
+  value: number;
+  measurement: Measurement;
+};
 
 export class UserEvent extends Listener<typeof Events.MessageCreate> {
   public constructor(context: PieceContext, options?: ListenerOptions) {
@@ -30,193 +36,228 @@ export class UserEvent extends Listener<typeof Events.MessageCreate> {
     )
       return;
 
-    // Get the units from the message
-    const units = UnitRegex.exec(message.content);
+    // Get all units from the message
+    let parsedUnit: Unit | null;
+    const units: Unit[] = [];
+    do {
+      // Iterate through array and convert the regex result to a Unit
+      parsedUnit = this.convert(UnitRegex.exec(message.content));
+      if (parsedUnit) units.push(parsedUnit);
+    } while (parsedUnit);
 
-    console.log(units);
+    // Build return message
+    let result = "";
+    for (const unit of units) {
+      result += `\`${unit.input}\` is equal to \`${
+        unit.value
+      } ${this.measureToName(unit.measurement)}s\`\n`;
+    }
 
-    // If the regex returned nothing, exit
-    if (!units) return;
+    return message.reply(result);
+  }
+
+  // Convert measure to a readable string name
+  private measureToName(measurement: Measurement): string {
+    switch (measurement) {
+      case Measurement.length:
+        return "meter";
+      case Measurement.volume:
+        return "liter";
+      case Measurement.mass:
+        return "gram";
+      case Measurement.time:
+        return "second";
+      case Measurement.unknown:
+        return "unknown";
+    }
+  }
+
+  // Convert regex input into a Unit
+  private convert(unit: RegExpExecArray | null): Unit | null {
+    if (!unit) return null;
 
     // Get the value and convert it
-    let value: number | null = parseFloat(units[1]);
-    let measurement: Measurements;
+    let value: number = parseFloat(unit[1]);
+    if (!value) return null;
+
+    let measurement: Measurement;
 
     // WARNING: This is awful code, proceed with caution
-    console.log(`${units[1]}, ${units[2]}`);
-    switch (units[2].toLowerCase()) {
+    switch (unit[2].toLowerCase()) {
       // Length
       case "mm":
       case "milimeter":
       case "milimeters":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value /= 1000;
         break;
       case "cm":
       case "centimeter":
       case "centimeters":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value /= 100;
         break;
       case "m":
       case "meter":
       case "meters":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         // Meter is our standard measure of length
         break;
       case "km":
       case "kilometer":
       case "kilometers":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value *= 1000;
         break;
       case '"':
       case "in":
       case "inch":
       case "inches":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value /= 39.37;
         break;
       case "'":
       case "ft":
       case "foot":
       case "feet":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value /= 3.281;
         break;
       case "yd":
       case "yard":
       case "yards":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value /= 1.094;
         break;
       case "mi":
       case "mile":
       case "miles":
-        measurement = Measurements.meter;
+        measurement = Measurement.length;
         value *= 1609;
         break;
       // Volume
       case "ml":
       case "milliliter":
-        measurement = Measurements.liter;
+        measurement = Measurement.length;
         value /= 1000;
         break;
       case "l":
       case "liter":
-        measurement = Measurements.liter;
+        measurement = Measurement.length;
         // Liter is our standard measure of volume
         break;
       case "cup":
       case "cups":
-        measurement = Measurements.liter;
+        measurement = Measurement.volume;
         value /= 4.227;
         break;
       case "pt":
       case "pint":
       case "pints":
-        measurement = Measurements.liter;
+        measurement = Measurement.volume;
         value /= 2.113;
         break;
       case "qt":
       case "quart":
       case "quarts":
-        measurement = Measurements.liter;
+        measurement = Measurement.volume;
         value /= 1.057;
         break;
       case "gal":
       case "gallon":
       case "gallons":
-        measurement = Measurements.liter;
+        measurement = Measurement.volume;
         value *= 3.785;
         break;
       // Mass
       case "mg":
       case "milligram":
       case "milligrams":
-        measurement = Measurements.gram;
+        measurement = Measurement.mass;
         value /= 1000;
         break;
       case "g":
       case "gram":
       case "grams":
-        measurement = Measurements.gram;
+        measurement = Measurement.mass;
         // Gram is our standard measure of mass/weight
         break;
       case "kg":
       case "kilogram":
       case "kilograms":
-        measurement = Measurements.gram;
+        measurement = Measurement.mass;
         value *= 100;
         break;
       // Weight
       case "oz":
       case "ounce":
       case "ounces":
-        measurement = Measurements.gram;
+        measurement = Measurement.mass;
         value *= 28.35;
         break;
       case "lb":
       case "pound":
       case "pounds":
-        measurement = Measurements.gram;
+        measurement = Measurement.mass;
         value *= 454;
         break;
       // Time
       case "ms":
       case "millisecond":
       case "milliseconds":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value /= 1000;
         break;
       case "s":
       case "second":
       case "seconds":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         // Second is our standard measure of time
         break;
       case "min":
       case "minute":
       case "minutes":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value *= 60;
         break;
       case "hr":
       case "hour":
       case "hours":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value *= 3600;
         break;
       case "day":
       case "days":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value *= 86400;
         break;
       case "week":
       case "weeks":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value *= 604800;
         break;
       case "month":
       case "months":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value *= 2.628e6;
         break;
       case "year":
       case "years":
-        measurement = Measurements.second;
+        measurement = Measurement.time;
         value *= 3.154e7;
         break;
       default:
-        measurement = Measurements.unknown;
-        value = null;
+        measurement = Measurement.unknown;
+        value = 0;
     }
 
-    if (measurement == Measurements.unknown) return;
+    if (measurement == Measurement.unknown) return null;
 
-    return message.reply(
-      `${units[0]} is equal to ${value} ${measurement.toString()}s`
-    );
+    return {
+      input: unit[0],
+      measurement,
+      value,
+    };
   }
 
   // Only enable if logCommands is true or we are in a dev enviornment
