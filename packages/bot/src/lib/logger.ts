ad8099ae46575c8b38f6dd05b9ca75aa489d660b
JosNun <josiah.nunemaker@gmail.com>
2021-10-02 02:12:16 +0000

    Move logger instance variables to constructor

--- packages/bot/src/lib/logger.ts
+++ packages/bot/src/lib/logger.ts
@@ -2,12 +2,6 @@ import winston from "winston";
 import { config } from "./config";
 
 export class BotLogger {
-  // prettyFormat is the log formatter used when in a development scenario
-  private prettyFormat: winston.Logform.Format = winston.format.simple();
-
-  // Default logging options, primarily handling logic depending on the environment
-  private defaultOptions: winston.LoggerOptions = {};
-
   // Used for general bot event logging
   public bot = winston.createLogger();
 
@@ -17,7 +11,7 @@ export class BotLogger {
   // TODO: Message logger for export to Loki/Elastic/Other
 
   public constructor(debug: boolean) {
-    this.prettyFormat = winston.format.combine(
+    const prettyFormat = winston.format.combine(
       winston.format.colorize(),
       winston.format.timestamp(),
       winston.format.printf(
@@ -31,10 +25,10 @@ export class BotLogger {
       ),
     );
 
-    this.defaultOptions = {
+    const defaultOptions = {
       level: debug ? "debug" : "info",
       format: debug
-        ? this.prettyFormat
+        ? prettyFormat
         : winston.format.combine(
             winston.format.timestamp(),
             winston.format.json(),
@@ -42,25 +36,15 @@ export class BotLogger {
       transports: [new winston.transports.Console()],
     };
 
-    this.bot.configure(
-      this.apply({
-        defaultMeta: { service: "bot" },
-      }),
-    );
+    this.bot.configure({
+      ...defaultOptions,
+      defaultMeta: { service: "bot" },
+    });
 
-    this.command.configure(
-      this.apply({
-        defaultMeta: { service: "command" },
-      }),
-    );
-  }
-
-  // Works much in the same way as applyConfig, just applies options for the logger
-  private apply(opts?: winston.LoggerOptions): winston.LoggerOptions {
-    return {
-      ...opts,
-      ...this.defaultOptions,
-    };
+    this.command.configure({
+      ...defaultOptions,
+      defaultMeta: { service: "command" },
+    });
   }
 }
 
