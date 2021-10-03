5ce9555446127d495e0c0ef2732ddf195eda6776
Anthony Cleary <aqc1@pct.edu>
2021-10-03 18:49:26 +0000

    Added error handling to decode command

--- packages/bot/src/commands/Util/decode.ts
+++ packages/bot/src/commands/Util/decode.ts
@@ -14,16 +14,14 @@ const reg_arr = [binary_reg, octal_reg, hex_reg, base64_reg];
 })
 export class UserCommand extends Command {
   public async run(message: Message, args: Args) {
-    let encoded = "";
-    while (await args.nextMaybe().exists) {
-      encoded += await args.nextMaybe().value;
-    }
+    const encoded = await args.rest("string");
     for (const reg of reg_arr) {
       if (reg.test(encoded)) {
         if (reg_arr.indexOf(reg) == 0) {
           try {
             let decoded = "";
-            const bin_char = encoded.match(new RegExp(".{1,8}", "g"));
+            const bin_char = encoded.match(new RegExp(".{1,8}", "g")) ?? "";
+            if (!bin_char || bin_char === []) throw new Error();
             for (const bin of bin_char) {
               decoded += String.fromCharCode(parseInt(bin, 2));
             }
@@ -34,7 +32,8 @@ export class UserCommand extends Command {
         } else if (reg_arr.indexOf(reg) == 1) {
           try {
             let decoded = "";
-            const octal_char = encoded.match(new RegExp(".{1,3}", "g"));
+            const octal_char = encoded.match(new RegExp(".{1,3}", "g")) ?? "";
+            if (!octal_char || octal_char === []) throw new Error();
             for (const octal of octal_char) {
               decoded += String.fromCharCode(parseInt(`${octal}`, 8));
             }
@@ -45,7 +44,8 @@ export class UserCommand extends Command {
         } else if (reg_arr.indexOf(reg) == 2) {
           try {
             let decoded = "";
-            const hex_char = encoded.match(new RegExp(".{1,2}", "g"));
+            const hex_char = encoded.match(new RegExp(".{1,2}", "g")) ?? "";
+            if (!hex_char || hex_char === []) throw new Error();
             for (const hex of hex_char) {
               decoded += String.fromCharCode(parseInt(`${hex}`, 16));
             }
@@ -55,7 +55,7 @@ export class UserCommand extends Command {
           }
         } else {
           try {
-            const buffer = new Buffer(encoded, "base64");
+            const buffer = Buffer.from(encoded, "base64");
             const decoded = buffer.toString("ascii");
             return send(message, `Base64: ${decoded}`);
           } catch (error) {
