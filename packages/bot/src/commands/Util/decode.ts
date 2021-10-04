69788ea6fe86f02f794835259c2a516f4d032420
Anthony Cleary <aqc1@pct.edu>
2021-10-04 00:07:55 +0000

    Changed naming conventions, changed command options to read from config.applyConfig

--- packages/bot/src/commands/Util/decode.ts
+++ packages/bot/src/commands/Util/decode.ts
@@ -2,53 +2,57 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { Args, Command, CommandOptions } from "@sapphire/framework";
 import { send } from "@sapphire/plugin-editable-commands";
 import type { Message } from "discord.js";
+import { config } from "../../lib/config";
 import {
-  binary_reg,
-  octal_reg,
-  hex_reg,
-  base64_reg,
+  BinaryRegex,
+  OctalRegex,
+  HexRegex,
+  BaseSixtyFourRegex,
 } from "../../lib/constants";
 
-const reg_arr = [binary_reg, octal_reg, hex_reg, base64_reg];
+const RegexArr = [BinaryRegex, OctalRegex, HexRegex, BaseSixtyFourRegex];
 
-@ApplyOptions<CommandOptions>({
-  description: "decode all the things",
-})
+@ApplyOptions<CommandOptions>(
+  config.applyConfig("decode", {
+    description: "decode all the things",
+  }),
+)
 export class UserCommand extends Command {
   public async run(message: Message, args: Args) {
     const encoded = await args.rest("string");
-    for (const reg of reg_arr) {
+    for (const reg of RegexArr) {
       if (reg.test(encoded)) {
-        if (reg_arr.indexOf(reg) == 0) {
+        if (RegexArr.indexOf(reg) == 0) {
           try {
             let decoded = "";
-            const bin_char = encoded.match(new RegExp(".{1,8}", "g")) ?? "";
-            if (!bin_char || bin_char === []) throw new Error();
-            for (const bin of bin_char) {
+            const BinCharacter = encoded.match(new RegExp(".{1,8}", "g")) ?? "";
+            if (!BinCharacter || BinCharacter === []) throw new Error();
+            for (const bin of BinCharacter) {
               decoded += String.fromCharCode(parseInt(bin, 2));
             }
             return send(message, `Binary: ${decoded}`);
           } catch (error) {
             continue;
           }
-        } else if (reg_arr.indexOf(reg) == 1) {
+        } else if (RegexArr.indexOf(reg) == 1) {
           try {
             let decoded = "";
-            const octal_char = encoded.match(new RegExp(".{1,3}", "g")) ?? "";
-            if (!octal_char || octal_char === []) throw new Error();
-            for (const octal of octal_char) {
+            const OctalCharacter =
+              encoded.match(new RegExp(".{1,3}", "g")) ?? "";
+            if (!OctalCharacter || OctalCharacter === []) throw new Error();
+            for (const octal of OctalCharacter) {
               decoded += String.fromCharCode(parseInt(`${octal}`, 8));
             }
             return send(message, `Octal: ${decoded}`);
           } catch (error) {
             continue;
           }
-        } else if (reg_arr.indexOf(reg) == 2) {
+        } else if (RegexArr.indexOf(reg) == 2) {
           try {
             let decoded = "";
-            const hex_char = encoded.match(new RegExp(".{1,2}", "g")) ?? "";
-            if (!hex_char || hex_char === []) throw new Error();
-            for (const hex of hex_char) {
+            const HexCharacter = encoded.match(new RegExp(".{1,2}", "g")) ?? "";
+            if (!HexCharacter || HexCharacter === []) throw new Error();
+            for (const hex of HexCharacter) {
               decoded += String.fromCharCode(parseInt(`${hex}`, 16));
             }
             return send(message, `Hexadecimal: ${decoded}`);
