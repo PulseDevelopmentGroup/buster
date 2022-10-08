a759f51633691451f9fbe3e4bf2fb941473ebe97
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2022-10-08 23:55:29 +0000

    First pass at making jpeg a context menu option

--- packages/bot/src/commands/Images/jpeg.ts
+++ packages/bot/src/commands/Images/jpeg.ts
@@ -1,109 +1,80 @@
-import { Message, MessageAttachment } from "discord.js";
+import { MessageAttachment } from "discord.js";
 import { isImageURL, getImageUrl } from "../../lib/utils";
 import jimp from "jimp";
-import { Args, Command, CommandOptions } from "@sapphire/framework";
+import {
+  ApplicationCommandRegistry,
+  Command,
+  CommandOptions,
+} from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
-import { send } from "@sapphire/plugin-editable-commands";
 import { config } from "../../lib/config";
+import { ApplicationCommandType } from "discord-api-types/v9";
+import { isMessageInstance } from "@sapphire/discord.js-utilities";
 
 @ApplyOptions<CommandOptions>(
   config.applyConfig("jpeg", {
+    name: "JPEG",
     description: "More JPEG. 'nuff said",
   }),
 )
 export class JpegCommand extends Command {
-  public async messageRun(msg: Message, args: Args) {
-    const target = !args.finished && (await args.rest("string"));
-    const mentioned = msg.mentions?.users?.first();
-    let imgUrl: string;
+  public override registerApplicationCommands(
+    registry: ApplicationCommandRegistry,
+  ) {
+    registry.registerContextMenuCommand((builder) =>
+      builder.setName(this.name).setType(ApplicationCommandType.Message),
+    );
+  }
 
-    if (!target) {
-      ////
-      //  If no target param exists
-      ////
-      const [lastMessage] = await msg.channel.messages.fetch({
-        before: msg.id,
-        limit: 1,
-      });
+  public override async contextMenuRun(
+    interaction: Command.ContextMenuInteraction,
+  ) {
+    if (
+      interaction.isMessageContextMenu() &&
+      isMessageInstance(interaction.targetMessage)
+    ) {
+      const msg = interaction.targetMessage;
+      const mentioned = msg.mentions.users.first();
+      let imgUrl: string;
 
-      if (!lastMessage) {
-        return send(msg, "Sorry, I can't find the last message :(");
+      if (mentioned) {
+        ////
+        //  If target param is a mention
+        ////
+        imgUrl = mentioned.displayAvatarURL().slice(0, -5);
+      } else {
+        const url = msg.attachments.first()?.url ?? getImageUrl(msg.content);
+        if (!url || !isImageURL(url)) {
+          return interaction.reply(
+            "The specified message doesn't appear to have any JPEGifiable attachments.",
+          );
+        }
+
+        imgUrl = url;
       }
 
-      const url =
-        lastMessage[1].attachments.first()?.url ??
-        getImageUrl(lastMessage[1].content);
+      try {
+        const attachment = await jimp.read(imgUrl).then((i) => {
+          return i
+            .posterize(config.json.commands.jpeg.vars.posterize)
+            .quality(config.json.commands.jpeg.vars.jpeg)
+            .getBufferAsync(jimp.MIME_JPEG)
+            .then((b) => {
+              return b;
+            });
+        });
 
-      if (!url) {
-        return send(
-          msg,
-          "Hmm... There doesn't appear to be an image in the last message. Try specifying a message ID.",
-        );
+        return interaction.reply({
+          files: [new MessageAttachment(attachment)],
+        });
+      } catch (e) {
+        msg.client.logger.error(e);
+        return interaction.reply({
+          content: "Unable to JPEGify the image D:",
+        });
       }
-
-      if (!isImageURL(url)) {
-        return send(
-          msg,
-          "Wat. I can't seem to recognize that attachment as an image D:",
-        );
-      }
-
-      imgUrl = url;
-    } else if (isImageURL(target)) {
-      ////
-      //  If target param is a URL
-      ////
-      imgUrl = target;
-    } else if (mentioned) {
-      ////
-      //  If target param is a mention
-      ////
-      imgUrl = mentioned.displayAvatarURL().slice(0, -5);
-    } else {
-      ////
-      //  If target param is not a URL
-      ////
-      const message = await msg.channel.messages
-        .fetch(target)
-        .catch(() => undefined);
-
-      if (!message) {
-        return send(msg, "I couldn't find a message with that ID.");
-      }
-
-      const msgAttachment = message.attachments.first();
-
-      if (!msgAttachment) {
-        return send(msg, "The specified message doesn't have any attachments.");
-      }
-
-      if (!isImageURL(msgAttachment.url)) {
-        return send(
-          msg,
-          "The specified message doesn't appear to have any JPEGifiable attachments.",
-        );
-      }
-
-      imgUrl = msgAttachment.url;
     }
 
-    try {
-      const attachment = await jimp.read(imgUrl).then((i) => {
-        return i
-          .posterize(config.json.commands.jpeg.vars.posterize)
-          .quality(config.json.commands.jpeg.vars.jpeg)
-          .getBufferAsync(jimp.MIME_JPEG)
-          .then((b) => {
-            return b;
-          });
-      });
-
-      return send(msg, {
-        files: [new MessageAttachment(attachment)],
-      });
-    } catch (e) {
-      msg.client.logger.error(e);
-      return send(msg, "Unable to JPEGify the image D:");
-    }
+    return;
   }
 }
